1cd8354f067bd377b3ba66a704ceb5328ab82c92
IT Compliance <compliance@amalitech.org>
2024-07-03 08:44:25 +0000

    feat:created a component to preview the editor content

---
+++ src/app/components/editor-preview/editor-preview.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-editor-preview',
+  standalone: true,
+  imports: [],
+  templateUrl: './editor-preview.component.html',
+  styleUrl: './editor-preview.component.scss'
+})
+export class EditorPreviewComponent {
+
+}
