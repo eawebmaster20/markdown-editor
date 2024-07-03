c577afcf464bb56ed59701bfb0bb12d167353ffc
eawebmaster20 <eaweb20@gmail.com>
2024-07-03 10:15:43 +0000

    feat: removed unwanted declaration of variable and imported the preview component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { EditorPreviewComponent } from './components/editor-preview/editor-preview.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,EditorPreviewComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  buttonName = 'Save Changes';
+  
 }
