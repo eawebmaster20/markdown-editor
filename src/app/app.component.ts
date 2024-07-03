5d0ef541f7670f53518c8495e8af34391ead4081
IT Compliance <compliance@amalitech.org>
2024-07-03 07:33:21 -0700

    Added Markup-pane UI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MarkdownPaneComponent } from './markdown-pane/markdown-pane.component'
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MarkdownPaneComponent] ,
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
---
+++ src/app/markdown-pane/markdown-pane.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-markdown-pane',
+  standalone: true,
+  imports: [],
+  templateUrl: './markdown-pane.component.html',
+  styleUrl: './markdown-pane.component.scss'
+})
+export class MarkdownPaneComponent {
+
+}
