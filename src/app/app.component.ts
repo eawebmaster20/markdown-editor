6e6059592200ea6b39cd1b79d307a477f4813fb9
eawebmaster20 <eaweb20@gmail.com>
2024-07-06 23:06:03 +0000

    feat: injected dependencies and added method calls in the OnInit hook

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { PreviewComponent } from './components/preview/preview.component';
 import { MarkdownPaneComponent } from './components/markdown-pane/markdown-pane.component';
@@ -7,6 +7,8 @@ import { ToolbarComponent } from './components/toolbar/toolbar.component';
 import { ConfirmDeleteModalComponent } from './components/confirm-delete-modal/confirm-delete-modal.component';
 import { CreateModalComponent } from './components/create-modal/create-modal.component';
 import { PreviewTogglerService } from './services/preview-toggler.service';
+import { CurrentDocumentService } from './services/current-document.service';
+import { DocumentsService } from './services/documents.service';
 
 
 @Component({
@@ -24,6 +26,29 @@ import { PreviewTogglerService } from './services/preview-toggler.service';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   toggleService = inject(PreviewTogglerService)
+  documentService = inject(DocumentsService);
+  currDocService = inject(CurrentDocumentService);
+  ngOnInit() {
+    if (
+      localStorage.getItem('docList') &&
+      localStorage.getItem('docList')!.length > 0
+    ) {
+      this.currDocService.documents = JSON.parse(localStorage.getItem('docList')!);
+      let docLength= this.currDocService.documents.length
+      this.currDocService.currDocument = this.currDocService.documents[docLength-1];
+      this.currDocService.updateText();
+      console.log(this.currDocService.currDocument);
+      return;
+    }
+    this.documentService.getDocuments().subscribe((data) => {
+      let docLength= data.length
+      this.currDocService.documents = data
+      this.currDocService.currDocument = this.currDocService.documents[docLength-1];
+      localStorage.setItem('docList', JSON.stringify(data));
+      this.currDocService.updateText();
+      console.log('blav bla bla lbal bla ');
+    });
+  }
 }
